a8ec00f002d4c2a107c9253f7e7e63eda66d130b
LVooo <61344766+LVooo@users.noreply.github.com>
2022-12-13 21:24:34 +1100

    sizeof；static；this；

--- static/demo/Project1/源.cpp
+++ static/demo/Project1/源.cpp
@@ -1,18 +1,25 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
-
-class Apple
+class A
 {
-	int i;
+    virtual void fun() {}
+};
+class B
+{
+    virtual void fun2() {}
+};
+class C : virtual public  A
+{
 public:
-	Apple()
-	{
-		i = 0;
-		cout << "Inside Constructor\n";
-	}
-	~Apple()
-	{
-		cout << "Inside Destructor\n";
-	}
+    virtual void fun3() {}
 };
 
+int main()
+{
+    /**
+     * @brief 8 8 16  派生类虚继承多个虚函数，会继承所有虚函数的vptr
+     */
+    cout << sizeof(A) << " " << sizeof(B) << " " << sizeof(C);
+
+    return 0;
+}
