dcf2721e3dead85d58f30855d1edfa9237ad8a25
LVooo <1371458538@qq.com>
2022-12-20 19:29:39 +1100

    2

--- static/demo/Project1/源.cpp
+++ static/demo/Project1/源.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class Student
+{
+public:
+	// 类中的属性和行为 我们统一称为成员
+	// 属性 成员属性 成员变量
+	// 行为 成员函数 成员方法
+
+
+	string name;
+	int number;
+
+	void showInfo()
+	{
+		cout << "学生的姓名是：" << name << " 编号为：" << number << endl;
+	}
+};
+
 int main()
 {
-	string  str2 = "Hello";
-	cout << true << endl;
+	Student s1;
+	s1.name = "小葛";
+	s1.number = 5332112;
+	
+	s1.showInfo();
 
-	int int_type;
-	cout << "输入" << endl;
-	cin >> int_type;
-	cout << "输出" << int_type << endl;
-	/*
-	* 暂停程序进程
-	*/
 	system("pause");
 	return 0;
 }
